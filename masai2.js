596ddfe866018bfd0e31bb3bd35abfa7a61b6368
riteshkumardev <sriteshkumar11022@gmail.com>
2022-03-27 19:09:01 +0530

    title updated

---
+++ masai2.js
@@ -0,0 +1,66 @@
+// number 1 Print "Masi School" on the console
+
+console.log("Masai School");
+
+// number 2 Print your name by it storing in a variable
+
+var num= "Ritesh kumar"
+ console.log(num);
+ 
+//  Number 3 Create a variable called name use it to print your father's name and your name
+
+var Father_name="Umesh Mandal"
+var My_name="Ritesh kumar"
+console.log(Father_name);
+console.log(My_name);
+
+// Number 4 Given the side of the square calculate its perimeter
+
+var side=3
+side=side*4
+console.log(side);
+
+// Number 5 Given three variable print their product
+
+var a="mobile "
+var b="headphone "
+var c= "charger "
+var product= a+b+c
+// console.log(product);
+
+// Number 6 Given a number find its cube
+
+var num=8;
+//  num=num**num
+var result =Math.pow(num,10)
+ console.log(result);
+ 
+//  number 7 Given two numbers in string format find their difference
+
+var num="5";
+var num1="6";
+console.log(num+num1);
+num= Number("5");
+num1=Number("6");
+console.log(num+num1);
+
+// number 8 Given the certain number of pencils find the number of pencils left behind if equally distributed between 3 children
+
+var pencils=30;
+var children=3
+ children=pencils/children
+ console.log(children);
+
+//  number 9 Given all the expenses find the total expense
+
+var bijliBil=1200;
+var waterBill=400;
+var roomRent=7000;
+var mobileBill=200;
+var total=bijliBil+waterBill+roomRent+mobileBill
+console.log("total expense = "+total);
+
+
+
+
+
