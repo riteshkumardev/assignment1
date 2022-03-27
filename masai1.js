2ef8215e748d2eda7fbd8f145d6095f6b506c051
amankumarsuman <amankumarsuman.ssjha143@gmail.com>
2022-03-27 15:15:59 +0530

    lecture1

---
+++ masai1.js
@@ -0,0 +1,103 @@
+var num= 1
+//type1
+console.log("Hell Masai");
+//type2
+var num="Hello Masai"
+console.log(num);
+// Number 2
+console.log("Masai School");
+ console.log("A Transformation in Education ");
+
+// Number 3
+
+var num= "Nrupul Dev "
+console.log(num);
+
+// Number 4 
+
+var age = 26;
+console.log(age);
+
+// Number 5
+
+var location= "kothra,baheri"
+ var city = "Darbhanga "
+var state ="Bihar"
+
+console.log(location)
+ console.log(city)
+ console.log(state)
+
+// Number 6
+
+ var gender = "male"
+ var height ="47 in "
+var food_preference=  "non-veg"
+
+ console.log(gender);
+console.log(height);
+console.log(food_preference);
+
+// Number 7
+
+ var num= "my name"
+ console.log(num);
+num =  "my father name"
+console.log(num);
+
+// Number 8
+
+var year = 2021
+var dbo = 1996
+ var age = year-dbo
+ console.log(age);
+
+// Number 9 
+
+var sub1=56
+var sub2=45
+var sub3=37
+var mark=sub1+sub1+sub3
+console.log(mark);
+
+// Number 10
+
+var l=27;
+var b=7;
+var lb=l*b
+console.log(lb);
+
+// Number 11
+
+var location= "kothra,baheri"
+var city = "Darbhanga "
+var state ="Bihar"
+var add = location+city+state
+console.log(add);
+
+// Number 12
+
+var month=26;
+var year =12
+console.log(month%year);
+
+// Number 13
+
+var amount=10000
+var year =2
+var int = amount*8/100*year
+var totale = amount+int
+console.log(totale);
+
+// Number 14
+
+var arry= [10,20,30,40,50,60]
+var add =0;
+var avg=0
+for(var i=0; i <arry.length;i++){
+  
+    add=add+arry[i]
+  
+}
+avg=add/arry.length
+console.log("averagr is =>",avg);
